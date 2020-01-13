a92af20fc0f5f7188df4aaf943843bd9500b6092
luankefei <luankefei@gmail.com>
2020-01-13 19:32:31 +0800

    backup font register

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/luankefei/golang-canvas
+
+go 1.13
+
+require (
+	github.com/tdewolff/canvas v0.0.0-20200112234221-d90d8eee919d
+	github.com/tdewolff/test v1.0.6
+)
---
+++ src/libs/font.go
@@ -0,0 +1,17 @@
+package libs
+
+import (
+	"github.com/tdewolff/canvas"
+	"github.com/tdewolff/test"
+)
+
+// @see https://github.com/tdewolff/canvas/blob/master/examples/html-canvas/main.go
+func RegisterFont() {
+	family := canvas.NewFontFamily("dejavu-serif")
+	family.LoadFontFile("font/DejaVuSerif.ttf", canvas.FontRegular)
+
+	// size float64, col color.Color, style FontStyle, variant FontVariant, deco ...FontDecorator
+	face := family.Face(12.0*ptPerMm, canvas.Black, canvas.FontRegular, canvas.FontNormal)
+	test.Float(t, face.fauxBold, 0.0)
+	test.T(t, face.boldness(), 400)
+}
