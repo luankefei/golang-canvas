314f0512e256733c579735aad4790b06d3a268ad
luankefei <luankefei@gmail.com>
2020-02-24 16:47:24 +0800

    test view

--- src/canvas/text.go
+++ src/canvas/text.go
@@ -32,20 +32,31 @@ func testLoadFont() {
 func testText() {
 	testLoadFont()
 
-	c := canvas.New(265, 90)
+	c := canvas.New(1000, 1000)
 	ctx := canvas.NewContext(c)
-	drawText(ctx, 5.0, 80.0, canvas.Left, canvas.Top, 10.0)
-	c.SavePNG("out.png", 5.0)
+
+	matrix := canvas.Identity.Translate(0, 500)
+	// .Rotate(180).ReflectY().
+
+	ctx.SetView(matrix)
+	// ctx.ComposeView(matrix)
+	// ctx.ResetView()
+
+	drawText(ctx, 0.0, 0.0, canvas.Left, canvas.Top, 0.0)
+
+	// savePng的第二个参数是canvas导出时放大的倍数
+	c.SavePNG("out.png", 1.0)
 }
 
 func drawText(c *canvas.Context, x, y float64, halign, valign canvas.TextAlign, indent float64) {
-	face := fontFamily.Face(6.0, color.Black, canvas.FontRegular, canvas.FontNormal)
+	face := fontFamily.Face(20.0, color.Black, canvas.FontRegular, canvas.FontNormal)
 	// phrase := "测试的文本一，phase,测试的文本一，phase,测试的文本一，phase,测试的文本一，phase, 测试的文本一，phase,测试的文本一，phase,测试的文本一，phase"
 	phrase := "hello, world,hello, world,hello, world,hello, world,hello, world,hello, world,hello, world,hello, world,hello, world,hello, world,hello, world,hello, world,hello, world,hello, world, yeah"
 
-	text := canvas.NewTextBox(face, phrase, 60.0, 0.0, halign, valign, indent, 4.0)
+	// box的宽高 0是auto
+	text := canvas.NewTextBox(face, phrase, 0.0, 0.0, halign, valign, indent, 0.0)
 	rect := text.Bounds()
-	rect.Y = 0.0
+	// rect.Y = 0.0
 	rect.H = -35.0
 	c.SetFillColor(canvas.Whitesmoke)
 	c.DrawPath(x, y, rect.ToPath())
